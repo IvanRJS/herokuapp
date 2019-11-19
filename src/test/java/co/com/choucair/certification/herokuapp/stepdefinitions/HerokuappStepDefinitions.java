31eb564e896670cf3ec64755aef75940df4497ce
Ivan Jimenez <ivanramirojs@gmail.com>
2019-11-19 12:32:58 -0500

    cambio feature

--- src/test/java/co/com/choucair/certification/herokuapp/stepdefinitions/HerokuappStepDefinitions.java
+++ src/test/java/co/com/choucair/certification/herokuapp/stepdefinitions/HerokuappStepDefinitions.java
@@ -12,72 +12,78 @@ import java.util.List;
 import static net.serenitybdd.screenplay.actors.OnStage.setTheStage;
 
 public class HerokuappStepDefinitions {
+
     @Before
     public void configuracionInicial() {
         setTheStage(new OnlineCast());
     }
 
-    @Dado("^que Ivan necesita registrar un nuevo doctor$")
-    public void queIvanNecesitaRegistrarUnNuevoDoctor() {
-        // Write code here that turns the phrase above into concrete actions
-        
-    }
+@Dado("^que Ivan necesita ingresar al sitio herokuapp$")
+public void queIvanNecesitaIngresarAlSitioHerokuapp() throws Exception {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+}
 
-    @Cuando("^el realiza el registro del doctor en el aplicativo de Administracion de Hospitales$")
-    public void elRealizaElRegistroDelDoctorEnElAplicativoDeAdministracionDeHospitales(List<DataModel> dataSet) {
-        // Write code here that turns the phrase above into concrete actions
-        // For automatic transformation, change DataTable to one of
-        // List<YourType>, List<List<E>>, List<Map<K,V>> or Map<K,V>.
-        // E,K,V must be a scalar (String, Integer, Date, enum etc)
-        
-    }
+@Dado("^que Ivan necesita registrar un nuevo doctor$")
+public void queIvanNecesitaRegistrarUnNuevoDoctor() throws Exception {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+}
 
-    @Entonces("^el verifica que se presente en pantalla el mensaje Datos guardados correctamente$")
-    public void elVerificaQueSePresenteEnPantallaElMensajeDatosGuardadosCorrectamente() {
-        // Write code here that turns the phrase above into concrete actions
-        
-    }
+@Cuando("^el realiza el registro del doctor en el aplicativo de Administracion de Hospitales$")
+public void elRealizaElRegistroDelDoctorEnElAplicativoDeAdministracionDeHospitales(DataTable arg1) throws Exception {
+    // Write code here that turns the phrase above into concrete actions
+    // For automatic transformation, change DataTable to one of
+    // List<YourType>, List<List<E>>, List<Map<K,V>> or Map<K,V>.
+    // E,K,V must be a scalar (String, Integer, Date, enum etc)
+    throw new PendingException();
+}
 
-    @Dado("^que Ivan necesita registrar un nuevo paciente$")
-    public void queIvanNecesitaRegistrarUnNuevoPaciente() {
-        // Write code here that turns the phrase above into concrete actions
-        
-    }
+@Entonces("^el verifica que se presente en pantalla el mensaje Datos guardados correctamente$")
+public void elVerificaQueSePresenteEnPantallaElMensajeDatosGuardadosCorrectamente() throws Exception {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+}
 
-    @Cuando("^el realiza el registro del paciente en el aplicativo de Administracion de Hospitales$")
-    public void elRealizaElRegistroDelPacienteEnElAplicativoDeAdministracionDeHospitales(List<DataModel> dataSet) {
-        // Write code here that turns the phrase above into concrete actions
-        // For automatic transformation, change DataTable to one of
-        // List<YourType>, List<List<E>>, List<Map<K,V>> or Map<K,V>.
-        // E,K,V must be a scalar (String, Integer, Date, enum etc)
-        
-    }
+@Dado("^que Ivan necesita registrar un nuevo paciente$")
+public void queIvanNecesitaRegistrarUnNuevoPaciente() throws Exception {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+}
 
-    @Entonces("^el verifica que en pantalla se presente el mensaje Datos guardados correctamente$")
-    public void elVerificaQueEnPantallaSePresenteElMensajeDatosGuardadosCorrectamente() {
-        // Write code here that turns the phrase above into concrete actions
-        
-    }
+@Cuando("^el realiza el registro del paciente en el aplicativo de Administracion de Hospitales$")
+public void elRealizaElRegistroDelPacienteEnElAplicativoDeAdministracionDeHospitales(DataTable arg1) throws Exception {
+    // Write code here that turns the phrase above into concrete actions
+    // For automatic transformation, change DataTable to one of
+    // List<YourType>, List<List<E>>, List<Map<K,V>> or Map<K,V>.
+    // E,K,V must be a scalar (String, Integer, Date, enum etc)
+    throw new PendingException();
+}
 
-    @Dado("^que Ivan necesita asistir al medico$")
-    public void queIvanNecesitaAsistirAlMedico() {
-        // Write code here that turns the phrase above into concrete actions
-        
-    }
+@Entonces("^el verifica que en pantalla se presente el mensaje Datos guardados correctamente$")
+public void elVerificaQueEnPantallaSePresenteElMensajeDatosGuardadosCorrectamente() throws Exception {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+}
 
-    @Cuando("^el realiza el agendamiento de una Cita$")
-    public void elRealizaElAgendamientoDeUnaCita(List<DataModel> dataSet) {
-        // Write code here that turns the phrase above into concrete actions
-        // For automatic transformation, change DataTable to one of
-        // List<YourType>, List<List<E>>, List<Map<K,V>> or Map<K,V>.
-        // E,K,V must be a scalar (String, Integer, Date, enum etc)
-        
-    }
+@Dado("^que Ivan necesita asistir al medico$")
+public void queIvanNecesitaAsistirAlMedico() throws Exception {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+}
 
-    @Entonces("^el verifica que se muestre en pantalla el mensaje Datos guardados correctamente$")
-    public void elVerificaQueSeMuestreEnPantallaElMensajeDatosGuardadosCorrectamente() {
-        // Write code here that turns the phrase above into concrete actions
-        
-    }
+@Cuando("^el realiza el agendamiento de una Cita$")
+public void elRealizaElAgendamientoDeUnaCita(DataTable arg1) throws Exception {
+    // Write code here that turns the phrase above into concrete actions
+    // For automatic transformation, change DataTable to one of
+    // List<YourType>, List<List<E>>, List<Map<K,V>> or Map<K,V>.
+    // E,K,V must be a scalar (String, Integer, Date, enum etc)
+    throw new PendingException();
+}
 
+@Entonces("^el verifica que se muestre en pantalla el mensaje Datos guardados correctamente$")
+public void elVerificaQueSeMuestreEnPantallaElMensajeDatosGuardadosCorrectamente() throws Exception {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+}
     }
--- src/test/resources/features/herokuapp.feature
+++ src/test/resources/features/herokuapp.feature
@@ -6,9 +6,12 @@ Característica: Aplicativo herokuapp
   Quiero realizar la solicitud de una cita médica
   A través del sistema de Administración de Hospitales
 
+  Antecedentes:
+    Dado que Ivan necesita ingresar al sitio herokuapp
+
   @Tag1
   Escenario: Realizar el Registro de un Doctor
-    Dado que Ivan necesita registrar un nuevo doctor
+    Y que Ivan necesita registrar un nuevo doctor
     Cuando el realiza el registro del doctor en el aplicativo de Administracion de Hospitales
       | nombre | apellidos | telefono | tipo_documento       | nro_documento |
       | john   | wick      | 555666   | Cédula de ciudadanía | 89012         |
@@ -17,7 +20,7 @@ Característica: Aplicativo herokuapp
 
   @Tag2
   Escenario: Realizar el Registro de un Paciente
-    Dado que Ivan necesita registrar un nuevo paciente
+    Y que Ivan necesita registrar un nuevo paciente
     Cuando el realiza el registro del paciente en el aplicativo de Administracion de Hospitales
       | nombre | apellidos | telefono | tipo_documento       | nro_documento | salud_prepagada |
       | john   | wick      | 555666   | Cédula de ciudadanía | 89012         | salud_prepagada |
@@ -27,7 +30,7 @@ Característica: Aplicativo herokuapp
 
   @Tag3
   Escenario: Realizar el Agendamiento de una Cita
-    Dado que Ivan necesita asistir al medico
+    Y que Ivan necesita asistir al medico
     Cuando el realiza el agendamiento de una Cita
       | dia_cita         | documento_paciente | documento_doctor | observaciones  |
       | 20 November 2019 | 89012              | 89012            | cita de prueba |
